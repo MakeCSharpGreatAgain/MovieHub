07ed12c523752663b0620fd10bd7ea89e78f705f
George221b <george221b@gmail.com>
2017-04-06 18:14:24 +0200

    Genre, Actor, Production, Director models Added

--- MovieHub/Models/Movie.cs
+++ MovieHub/Models/Movie.cs
@@ -5,8 +5,16 @@ using System.Web;
 
 namespace MovieHub.Models
 {
-    public class Movie // This will later become MovieDTO.cs (just as it is), and this fail will change, in order to pass correctly with the other models.
+    public class Movie
     {
+        public Movie()
+        {
+            this.Genres = new HashSet<Genre>();
+            this.Actors = new HashSet<Actor>();
+        }
+
+        public int Id { get; set; }
+
         public string Title { get; set; }
 
         public int Year { get; set; }
@@ -17,17 +25,19 @@ namespace MovieHub.Models
 
         public string Runtime { get; set; }
 
-        public string Genre { get; set; } // This will become freign key. Many to many
+        public virtual ICollection<Genre> Genres { get; set; }
 
-        public string Director { get; set; } // This will be a foreign key. One to Many
+        public int DirectorId { get; set; }
 
-        public string Actors { get; set; } // This will be foreign key. Many to many
+        public virtual Director Director { get; set; }
+
+        public virtual ICollection<Actor> Actors { get; set; }
 
         public string Plot { get; set; }
 
-        public string Language { get; set; } // There could be more than 1.
+        public string[] Language { get; set; } // There could be more than 1.
 
-        public string Awards { get; set; }
+        public string Awards { get; set; } // Simple string E.g. Nominated for 2 Oscars. Another 50 wins & 96 nominations.
 
         // public string Poster { get; set; } // This will be a link. Still no idea how to implement this!?
 
@@ -35,6 +45,8 @@ namespace MovieHub.Models
 
         public long imdbVotes { get; set; }
 
-        public string Production { get; set; } // This will be a foreign key. Many to many.
+        public int ProductionId { get; set; }
+
+        public virtual Production Production { get; set; }
     }
 }
