9023675c3b260e8e9ce10aa44110c29a270dcfa7
George221b <george221b@gmail.com>
2017-04-17 16:18:43 +0200

    EditReview Controller and View added. Minor design changes.

---
+++ MovieHub/Controllers/ReviewController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieHub.Controllers
+{
+    public class ReviewController : Controller
+    {
+        // GET: Review
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Movie/Edit
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
