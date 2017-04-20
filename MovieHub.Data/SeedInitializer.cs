f90a4e7f5f9b52eafd72ba2166b86a3a6b34b849
George221b <george221b@gmail.com>
2017-04-20 11:48:00 +0200

    DeleteReview, ListAll Views reworked

--- MovieHub.Data/SeedInitializer.cs
+++ MovieHub.Data/SeedInitializer.cs
@@ -95,35 +95,9 @@
             }
         }
 
-        //private static List<string> GetDistinctActorNames(string json)
-        //{
-        //    List<CsvActorsDTO> actorNamesCSV = JsonConvert.DeserializeObject<List<CsvActorsDTO>>(json);
-        //    List<string> actorNames = new List<string>();
-
-        //    foreach (var item in actorNamesCSV)
-        //    {
-        //        string[] actors = item.Actors.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
-        //        actorNames.AddRange(actors);
-        //    }
-
-        //    return actorNames.Distinct().ToList();
-        //}
 
         private void ImportProductions()
         {
-            //string json = File.ReadAllText(filePath);
-
-            //List<Production> productions = JsonConvert.DeserializeObject<List<Production>>(json);
-
-            //foreach (var prod in productions)
-            //{
-            //    if (!context.Productions.Any(p => p.Name == prod.Name))
-            //    {
-            //        context.Productions.Add(prod);
-            //        context.SaveChanges();
-            //    }
-            //}
-
             ICollection<Production> productions = JsonImport.ImportProductions();
             using (MovieDbContext context = new MovieDbContext())
             {
@@ -134,14 +108,6 @@
 
         private void ImportGenres()
         {
-            //string json = File.ReadAllText(filePath);
-
-            //List<string> genreNames = GetDistinctGenreNames(json);
-            //List<Genre> genres = Mapper.Map<List<string>, List<Genre>>(genreNames);
-
-            //context.Genres.AddRange(genres);
-            //context.SaveChanges();
-
             ICollection<Genre> genres = JsonImport.ImportGenres();
             using (MovieDbContext context = new MovieDbContext())
             {
@@ -150,49 +116,8 @@
             }
         }
 
-        //private static List<string> GetDistinctGenreNames(string json)
-        //{
-        //    List<CsvGenresDTO> genresCSVs = JsonConvert.DeserializeObject<List<CsvGenresDTO>>(json);
-
-        //    List<string> genresList = new List<string>();
-
-        //    foreach (var g in genresCSVs)
-        //    {
-        //        string[] genres = g.GenresCSV.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-        //        genresList.AddRange(genres);
-        //    }
-
-        //    genresList = genresList.Distinct().ToList();
-        //    return genresList;
-        //}
-
-        //private static List<string> GetDistinctDirectorNames(string json)
-        //{
-        //    List<CsvDirectorsDTO> directorNamesCSV = JsonConvert.DeserializeObject<List<CsvDirectorsDTO>>(json);
-
-        //    List<string> directorNames = new List<string>();
-        //    foreach (var csv in directorNamesCSV)
-        //    {
-        //        string[] names = csv.Directors.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-
-        //        directorNames.AddRange(names);
-        //    }
-
-        //    directorNames = directorNames.Distinct().Select(d => d.Trim()).ToList();
-
-        //    return directorNames;
-        //}
-
         private void ImportDirectors()
         {
-            //string json = File.ReadAllText(filePath);
-
-            //List<string> directorNames = GetDistinctDirectorNames(json);
-            //List<Director> directors = Mapper.Map<List<string>, List<Director>>(directorNames);
-
-            //context.Directors.AddRange(directors);
-            //context.SaveChanges();
-
             ICollection<Director> directors = JsonImport.ImportDirectors();
             using (MovieDbContext context = new MovieDbContext())
             {
@@ -204,42 +129,6 @@
         private void ImportMovies()
         {
 
-
-            //string moviesJson = File.ReadAllText(filePath);
-
-            //List<MovieDTO> movieDTOs = JsonConvert.DeserializeObject<List<MovieDTO>>(moviesJson,
-            //    new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
-            //foreach (var movieDTO in movieDTOs)
-            //{
-            //    List<Genre> genres = GetGenresByName(context, movieDTO.Genres);
-            //    Director director = GetDirectorByName(context, movieDTO.DirectorName);
-            //    List<Actor> actors = GetActorsByName(context, movieDTO.ActorNames);
-            //    string[] languages = movieDTO.Language.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            //    Production production = GetProductionByName(context, movieDTO.Production);
-
-            //    Movie movie = new Movie()
-            //    {
-            //        Title = movieDTO.Title,
-            //        Awards = movieDTO.Awards,
-            //        Director = director,
-            //        Genres = genres,
-            //        Actors = actors,
-            //        ImdbRating = movieDTO.imdbRating,
-            //        Language = languages,
-            //        Plot = movieDTO.Plot,
-            //        PosterUrl = movieDTO.Poster,
-            //        Year = movieDTO.Year,
-            //        Runtime = movieDTO.Runtime,
-            //        Production = production,
-            //        Released = movieDTO.Released,
-            //        Rated = movieDTO.Rated
-            //    };
-
-            //    context.Movies.Add(movie);
-            //}
-
-            //context.SaveChanges();
-
             using (MovieDbContext context = new MovieDbContext())
             {
                 ICollection<Movie> movies = JsonImport.ImportMovies(context);
@@ -248,49 +137,6 @@
                 context.SaveChanges();
             }
         }
-
-        //private Production GetProductionByName(MovieDbContext context, string productionName)
-        //{
-        //    return context.Productions.FirstOrDefault(p => p.Name == productionName);
-        //}
-
-        //public static List<Actor> GetActorsByName(MovieDbContext context, string actorsCSV)
-        //{
-        //    string[] actorNames = actorsCSV
-        //        .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
-        //        .Select(a => a.Trim())
-        //        .ToArray();
-        //    List<Actor> actorsList = new List<Actor>();
-
-        //    foreach (var actorName in actorNames)
-        //    {
-        //        Actor actor = context.Actors.FirstOrDefault(a => a.Name == actorName);
-        //        actorsList.Add(actor);
-        //    }
-
-        //    return actorsList;
-        //}
-
-        //public static Director GetDirectorByName(MovieDbContext context, string directorName)
-        //{
-        //    return context.Directors
-        //        .FirstOrDefault(d => d.Name == directorName);
-        //}
-
-        //public static List<Genre> GetGenresByName(MovieDbContext context, string genresCSV)
-        //{
-        //    string[] genresNames = genresCSV.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-        //    List<Genre> genresList = new List<Genre>();
-
-        //    foreach (var genreName in genresNames)
-        //    {
-        //        Genre genre = context.Genres.FirstOrDefault(g => g.Name == genreName);
-        //        genresList.Add(genre);
-        //    }
-
-        //    return genresList;
-        //}
     }
 }
 
